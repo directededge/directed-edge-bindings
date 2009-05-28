b313ab3faf52529f4067571fcb91ef64f4a0da98
Scott Wheeler <scott@directededge.com>
2009-05-28 23:56:17 +0200

    Start adding outline of PHP bindings.

---
+++ PHP/DirectedEdge.php
@@ -0,0 +1,155 @@
+<?php
+
+class DirectedEdgeResource
+{
+    private $base;
+
+    public function __construct($base)
+    {
+        $this->base = $base;
+    }
+    
+    public function path($path)
+    {
+        return "$this->base/$path";
+    }
+
+    public function __toString()
+    {
+        return $this->base;
+    }
+}
+
+class DirectedEdgeDatabase
+{
+    private $resource;
+
+    public function __construct($name, $password, $protocol = 'http')
+    {
+        $host = $_ENV['DIRECTEDEDGE_HOST'];
+
+        if(!$host)
+        {
+            $host = 'webservices.directededge.com';
+        }
+
+        $base = "$protocol://$name:$password@$host/api/v1/$name";
+
+        $this->resource = new DirectedEdgeResource($base);
+    }
+
+    public function resource()
+    {
+        return $this->resource;
+    }
+
+    public function import($fileName)
+    {
+
+    }
+}
+
+class DirectedEdgeItem
+{
+    private $database;
+    private $id;
+    private $links = array();
+    private $tags = array();
+    private $properties = array();
+    private $cached = false;
+
+    public function __construct($database, $id)
+    {
+        $this->database = $database;
+        $this->id = $id;
+    }
+
+    public function name()
+    {
+        return $this->id;
+    }
+
+    public function tags()
+    {
+        read();
+        return $this->tags;
+    }
+
+    public function properties()
+    {
+        read();
+        return $this->properties;
+    }
+
+    public function property($name)
+    {
+        read();
+        return $this->properties[$name];
+    }
+
+    public function setProperty($name, $value)
+    {
+        $this->properties[$name] = $value;
+    }
+
+    public function clearProperty($name)
+    {
+        unset($this->properties[$name]);
+    }
+
+    public function destroy()
+    {
+
+    }
+
+    public function linkTo($other, $weight = 0)
+    {
+        $this->links[$other] = $weight;
+    }
+
+    public function unlinkFrom($other)
+    {
+        unset($this->links[$other]);
+    }
+
+    public function weightFor($other)
+    {
+        return $this->links[$other];
+    }
+
+    public function addTag($tag)
+    {
+        $this->tags[] = $tag;
+    }
+
+    public function removeTag($tag)
+    {
+        function filter($item) { return $item == $tag; }
+        $this->tags = array_filter($this->tags, filter);
+    }
+
+    private function read()
+    {
+        if($this->cached)
+        {
+            return;
+        }
+
+        $this->cached = true;
+    }
+}
+
+class DirectedEdgeExporter
+{
+
+}
+
+$database = new DirectedEdgeDatabase('testdb', 'test');
+
+$resource = new DirectedEdgeResource($database->resource());
+
+echo $resource->path('foo') . "\n";
+
+echo $database->resource()->path("foo") . "\n";
+
+?>
