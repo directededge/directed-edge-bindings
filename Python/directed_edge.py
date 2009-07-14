714d2c44f832f5f76e47bb29be7c59887d99a01b
Scott Wheeler <scott@directededge.com>
2009-07-14 02:09:49 -0700

    Get some basic reading working.

--- Python/directed_edge.py
+++ Python/directed_edge.py
@@ -1,6 +1,30 @@
+# Copyright (C) 2009 Directed Edge Ltd.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import os
 import urllib2
 import httplib2
+import xml.dom.minidom
 
 class Resource:
     def __init__(self, base_url, user=None, password=None):
@@ -12,7 +36,12 @@ class Resource:
     def path(self, sub=""):
         return self.base_url + "/" + urllib2.quote(sub)
 
-#    def get(sub=""):
+    def get(self, sub=""):
+        response, content = self.http.request(self.path(sub), "GET")
+        return content
+
+    def put(self, data, sub=""):
+        response, content = self.http.request(self.path(sub), "PUT", data)
 
 class Database:
     def __init__(self, name, password="", protocol="http"):
@@ -23,4 +52,31 @@ class Database:
         self.name = name
         self.resource = Resource("http://%s/api/v1/%s" % (host, name), name, password)
 
+    def import_from_file(self, file_name):
+        file = open(file_name, "r")
+        data = file.read()
+        
+        self.resource.put(data)
 
+class Item:
+    def __init__(self, database, id):
+        self.database = database
+        self.id = id
+
+    def name(self):
+        return self.id
+
+    def links(self):
+        values = []
+        for linkNode in self.__document().getElementsByTagName("link"):
+            values.append(linkNode.firstChild.data)
+        return values
+
+    def tags(self):
+        values = []
+        for tagNode in self.__document().getElementsByTagName("tag"):
+            values.append(tagNode.firstChild.data)
+        return values
+
+    def __document(self):
+        return xml.dom.minidom.parseString(self.database.resource.get(self.id))
