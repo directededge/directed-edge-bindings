85a63e07ab474d3aea9021886962666f97b8b40b
Scott Wheeler <scott@directededge.com>
2009-09-02 13:23:41 -0700

    Get basic resource reading working for the database.

--- Java/src/com/directededge/Database.java
+++ Java/src/com/directededge/Database.java
@@ -1,39 +1,77 @@
 /*
- * Copyright (C) 2008 Directed Edge, Inc.
+ * Copyright (C) 2009 Directed Edge, Inc.
  */
 
 package com.directededge;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.restlet.Client;
+import org.restlet.data.ChallengeResponse;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.data.Method;
 import org.restlet.data.Protocol;
-
+import org.restlet.data.Request;
 
 public class Database
 {
-    private String username;
+    private String name;
     private String password;
-
-    private final String protocol = "http";
-    private final String host = "webservices.directededge.com";
-
+    private String host;
+    private String protocol;
     private Client client;
 
     public Database(String username, String password)
     {
-        this.username = username;
+        protocol = "http";
+        name = username;
         this.password = password;
-        this.client = new Client(Protocol.HTTP);
+
+        host = System.getenv("DIRECTEDEDGE_HOST");
+        
+        if(host == null)
+        {
+            // host = "webservices.directededge.com";
+            host = "localhost";
+        }
+
+        client = new Client(Protocol.HTTP);
     }
 
     public String get()
     {
+        return get("");
+    }
+
+    public String get(String resource)
+    {
+        Request request = new Request(Method.GET, url(resource));
+        request.setChallengeResponse(
+                new ChallengeResponse(ChallengeScheme.HTTP_BASIC, name, password));
         try
         {
-            return client.get(protocol + "://" + host).getEntity().getText();
+            System.out.println(url(resource));
+            return client.handle(request).getEntity().getText();
         }
         catch(IOException ex)
         {
+            return "";
+        }
+    }
+
+    private String url(String resource)
+    {
+        try
+        {
+            URL url = new URL(protocol, host, "/api/v1/" + name + "/" + resource);
+            return url.toString();
+        }
+        catch (MalformedURLException ex)
+        {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
             return "";
         }
     }
--- Java/src/com/directededge/Main.java
+++ Java/src/com/directededge/Main.java
@@ -2,10 +2,6 @@ package com.directededge;
 
 public class Main
 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
         Database db = new Database("testdb", "test");
