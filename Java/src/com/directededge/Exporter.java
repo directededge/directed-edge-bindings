b1a9ed86803a89ec3473091a921f17ccadee99fa
Scott Wheeler <scott@directededge.com>
2009-09-04 18:25:37 -0500

    Actually write the exported file.

--- Java/src/com/directededge/Exporter.java
+++ Java/src/com/directededge/Exporter.java
@@ -4,17 +4,16 @@
 
 package com.directededge;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Exporter
 {
     private Database database;
-    private OutputStreamWriter output;
+    private BufferedWriter output;
 
     public Exporter(String fileName)
     {
@@ -22,7 +21,7 @@ public class Exporter
 
         try
         {
-            output = new OutputStreamWriter(new FileOutputStream(fileName));
+            output = new BufferedWriter(new FileWriter(fileName));
             output.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n");
             output.write("<directededge version=\"0.1\">\n");
         }
@@ -54,6 +53,7 @@ public class Exporter
         try
         {
             output.write("</directededge>\n");
+            output.close();
         }
         catch (IOException ex)
         {
