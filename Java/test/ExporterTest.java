c302de6477f18279ad33913cff50bda36a18bdab
Scott Wheeler <scott@directededge.com>
2009-09-05 11:39:08 -0500

    Add test skeleton.

---
+++ Java/test/ExporterTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author scott
+ */
+public class ExporterTest
+{
+    public ExporterTest()
+    {
+
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception
+    {
+
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception
+    {
+
+    }
+
+    @Before
+    public void setUp()
+    {
+
+    }
+
+    @After
+    public void tearDown()
+    {
+
+    }
+
+
+    @Test
+    public void exportFile()
+    {
+
+    }
+
+}
