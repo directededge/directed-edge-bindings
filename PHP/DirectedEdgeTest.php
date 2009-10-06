f52324f58148c6c24543664bcd2e7a9bb866a379
Scott Wheeler <scott@directededge.com>
2009-10-06 17:06:18 +0200

    Add a skeleton of tests mirroing the Python tests

---
+++ PHP/DirectedEdgeTest.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+
+class QueryTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+
+    }
+
+    public function testLinks()
+    {
+
+    }
+
+    public function testTags()
+    {
+
+    }
+
+    public function testRelated()
+    {
+
+    }
+
+    public function testProperties()
+    {
+
+    }
+
+    public function testSave()
+    {
+
+    }
+
+    public function testExport()
+    {
+
+    }
+
+    public function testAdd()
+    {
+
+    }
+
+    public function testLinkTypes()
+    {
+
+    }
+}
+
+?>
