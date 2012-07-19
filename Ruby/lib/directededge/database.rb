9d340dafc19293f61ee07985ccefe9a28ba3e1f5
Scott Wheeler <scott@directededge.com>
2012-07-19 03:07:45 +0200

    Start working in a database and item skeleton

---
+++ Ruby/lib/directededge/database.rb
@@ -0,0 +1,38 @@
+# Copyright (C) 2012 Directed Edge, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+require 'rubygems'
+require 'rest-client'
+
+module DirectedEdge
+  class Database
+    attr_reader :resource
+    def initialize(name, password, options = {})
+      host = options[:host] || ENV['DIRECTEDEDGE_HOST'] || 'webservices.directededge.com'
+      protocol = options[:protocol] || 'http'
+      url = "#{protocol}://#{name}:#{password}@#{host}/api/v1/#{name}"
+      options[:timeout] ||= 10
+      @resource = RestClient::Resource.new(url, options)
+    end
+  end
+end
