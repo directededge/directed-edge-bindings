0d8bc51ec8a8760d4bb2eeff571b6a342be001eb
Scott Wheeler <scott@directededge.com>
2012-07-25 20:22:12 +0200

    Organize imports

--- Ruby/lib/directededge.rb
+++ Ruby/lib/directededge.rb
@@ -21,6 +21,9 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+require 'libxml'
+require 'rest_client'
+
 require 'directededge/link'
 require 'directededge/xml'
 require 'directededge/resource'
--- Ruby/lib/directededge/database.rb
+++ Ruby/lib/directededge/database.rb
@@ -21,9 +21,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-require 'rubygems'
-require 'rest-client'
-
 module DirectedEdge
   class Database
     attr_reader :resource, :name
