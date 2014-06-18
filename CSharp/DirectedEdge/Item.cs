44d469a0fa9dcb2b65977b8e4b5be5dabfc0f639
Scott Wheeler <scott@directededge.com>
2014-06-18 22:24:22 +0200

    Use an actual Link class

--- CSharp/DirectedEdge/Item.cs
+++ CSharp/DirectedEdge/Item.cs
@@ -9,14 +9,14 @@ namespace DirectedEdge
         public Database Database { get; private set; }
         public string Id { get; private set; }
         public Resource Resource { get; private set; }
-        public Dictionary<string, int> Links { get; private set; }
+        public List<Link> Links { get; private set; }
 
         public Item(Database database, string id)
         {
             Database = database;
             Id = id;
             Resource = database.Resource.Child(id);
-            Links =  new Dictionary<string, int>();
+            Links = new List<Link>();
         }
 
         public void Load()
@@ -26,7 +26,10 @@ namespace DirectedEdge
             foreach(XmlNode node in doc.GetElementsByTagName("link"))
             {
                 XmlAttribute weight = node.Attributes["weight"];
-                Links.Add(node.InnerText, weight == null ? 0 : Convert.ToInt32(weight.Value));
+                XmlAttribute type = node.Attributes["type"];
+                Links.Add(new Link(node.InnerText,
+                        type == null ? null : node.InnerText,
+                        weight == null ? 0 : Convert.ToInt32(weight.Value)));
             }
         }
     }
---
+++ CSharp/DirectedEdge/Link.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DirectedEdge
+{
+    public class Link
+    {
+        public string Target { get; set; }
+        public string Type { get; set; }
+        public int Weight { get; set; }
+
+        public Link(string target, string type = null, int weight = 0)
+        {
+            Target = target;
+            Type = type;
+            Weight = weight;
+        }
+
+        public Link(string target, int weight) : this(target, null, weight)
+        {
+
+        }
+    }
+}
+
